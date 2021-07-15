a411c4bc75e773d635b128dd42a6f654406a0134
Глеб <glebighlebig@gmail.com>
2021-07-15 18:07:16 +0300

    удалили черепаху

--- шпкф.py
+++ шпкф.py
@@ -1,44 +1,42 @@
 import random
-import os
-import turtle
 import pygame
-did ="дигл деректива"
-scar ="скар наёмник"
-dib ="дигл бронзовый"
-dlor ="драгонлор"
+
+did = "дигл деректива"
+scar = "скар наёмник"
+dib = "дигл бронзовый"
+dlor = "драгонлор"
 drop = random.randint(1, 100)
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 menu = pygame.image.load("Безымянный.png")
-menu = pygame.transform.scale(menu,(800,600))
+menu = pygame.transform.scale(menu, (800, 600))
 loading = pygame.image.load("thumb-1920-895715.png")
-loading = pygame.transform.scale(loading,(800,600))
+loading = pygame.transform.scale(loading, (800, 600))
 fon = pygame.image.load("14160044614l.jpg")
-fon = pygame.transform.scale(fon,(800,600))
+fon = pygame.transform.scale(fon, (800, 600))
 chfon = pygame.image.load("1614281824_11-p-chernii-fon-kartinka-bez-risunka-13.jpg")
-chfon = pygame.transform.scale(chfon,(400,600))
+chfon = pygame.transform.scale(chfon, (400, 600))
 case = pygame.image.load("img-YyGw2L5PVlcBNJRQ-w1370.png")
-case = pygame.transform.scale(case,(260,230))
+case = pygame.transform.scale(case, (260, 230))
 open = pygame.image.load("ккк.jpg")
-open = pygame.transform.scale(open,(150,30))
+open = pygame.transform.scale(open, (150, 30))
 pygame.init()
-screen.blit(menu,(0,0))
+screen.blit(menu, (0, 0))
 x1 = 255
-x2 =488
-y1 =513
-y2 =547
+x2 = 488
+y1 = 513
+y2 = 547
 x3 = 319
-x4 =469
-y3 =499
-y4 =528
+x4 = 469
+y3 = 499
+y4 = 528
 # screen2 =pygame.display.set_mode((800,600))
 pygame.display.flip()
-
 
 while True:
     pos = pygame.mouse.get_pos()
 
     for e in pygame.event.get():
-        if e.type==pygame.QUIT:
+        if e.type == pygame.QUIT:
             exit()
         if e.type == pygame.MOUSEBUTTONDOWN:
             if x3 < pos[0] < x4 and y3 < pos[1] < y4:
@@ -48,7 +46,7 @@ while True:
                 x2 = 0
                 y1 = 0
                 y2 = 0
-                screen.blit(loading,(0,0))
+                screen.blit(loading, (0, 0))
                 pygame.display.flip()
                 pygame.time.delay(3000)
                 screen.blit(fon, (0, 0))
@@ -112,7 +110,7 @@ while True:
 # method_name()
 
 
-#if 1 <= drop <= 100:
+# if 1 <= drop <= 100:
 # elif 6 <= drop <= 300:
 #     print(did)
 # elif 301 <= drop <= 500:
@@ -120,5 +118,3 @@ while True:
 # elif 501 <= drop <= 1000:
 #     print(scar)
 # print(drop)
-
-
